81203a87b3b73548c6fc8bde91e02b7a5c2e33a3
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-02-14 10:53:51 +0100

    carousel

--- client/src/Carousel/Carousel.js
+++ client/src/Carousel/Carousel.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Carousel.css";
 import { Link } from 'react-router-dom';
+import logo from './carousel.jpeg'
 
 function Carousel() {
     return(
         <>
-            <img src='../../public/carousel.jpeg' alt= "logo" className="carousel" />
+            <img src={logo} alt= "logo" className="carousel" />
 
             <p>VIBREZ SUR NOS PLAGES CAPVERDIENNES</p>
         </>

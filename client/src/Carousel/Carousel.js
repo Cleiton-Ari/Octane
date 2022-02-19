179dd99489bcf01507b03b751460bf09d83cbad9
frenchToast <juliekogan1@gmail.com>
2022-02-19 16:59:17 +0100

    carousel

--- client/src/Carousel/Carousel.js
+++ client/src/Carousel/Carousel.js
@@ -2,15 +2,27 @@ import React from "react";
 import "./Carousel.css";
 import { Link } from 'react-router-dom';
 import logo from './carousel.jpeg'
+import { Slide } from 'react-slideshow-image'
+
+const slideImages = [
+  "Carousel/flyboarder.jpg",
+  "Carousel/jetstanding.jfif",
+  "Carousel/carousel.jpeg",
+];
+// a refaire
 
 function Carousel() {
-    return(
-        <div>
-            <img src={logo} alt= "logo" className="carousel" />
+    return (
+      <div>
+        <Slide>
+          <div>${slideImages[0]}</div>
+          <div>${slideImages[1]}</div>
+          <div>${slideImages[2]}</div>
 
-            <p>VENEZ VIBREZ SUR NOS PLUS PLAGES C</p>
-        </div>
-    )
+          <p>VENEZ VIBREZ SUR NOS PLUS PLAGES C</p>
+        </Slide>
+      </div>
+    );
    
 }
 

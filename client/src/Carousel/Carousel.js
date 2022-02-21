35c9ff3221e604d1255e5b88386d7b564ccc2b7e
frenchToast <juliekogan1@gmail.com>
2022-02-21 21:55:57 +0100

    carouselWIP

--- client/src/Carousel/Carousel.js
+++ client/src/Carousel/Carousel.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "./Carousel.css";
 import { Link } from 'react-router-dom';
-import logo from './carousel.jpeg'
+import {logo} from './carousel.jpeg'
 import { Slide } from 'react-slideshow-image'
 
-const slideImages = [
-  "Carousel/flyboarder.jpg",
-  "Carousel/jetstanding.jfif",
-  "Carousel/carousel.jpeg",
-];
+/*const slideImages = [
+  "./Carousel/flyboarder.jpg",
+  "./Carousel/jetstanding.jfif",
+  "/.Carousel/carousel.jpeg",
+];*/
 // a refaire
 
 function Carousel() {
     return (
-      <div>
+      <div className="slideContainer">
         <Slide>
-          <div>${slideImages[0]}</div>
-          <div>${slideImages[1]}</div>
-          <div>${slideImages[2]}</div>
+          <img src="./Carousel/flyboarder.jpg" alt=""/>
+          
 
           <p>VENEZ VIBREZ SUR NOS PLUS PLAGES C</p>
         </Slide>

aed27100214a4e7b05bea306d99c130eca39508c
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-02-21 22:01:33 +0100

    quickaccess

--- client/src/QuickAccess/QuickAccess.js
+++ client/src/QuickAccess/QuickAccess.js
@@ -4,37 +4,27 @@ import logo1 from './jet.png';
 import logo2 from './capvert.png';
 import {Link} from 'react-router-dom';
 import './QuickAccess.css';
- 
-
-
-//TODO
-/*
-declare routes panier, jet, capvert => reactr avec les cmpsts (APP.js) + css
-panier => class component avec state.
-
-*/
-
 
 
 const QuickAccess = () => {
     return (
     <div className="access">
         <Link to="/panier">
-            <div className='panier'>
+            <div className='panier imgs'>
                 <img src={logo} alt="panier" /> 
                 <p> Reservation facile</p>
             </div>
         </Link>
 
         <Link to="/jet">
-            <div className='jet'>
+            <div className='jet imgs'>
                 <img src={logo1} alt="jet" />
                 <p>100% Experience</p>
             </div>
         </Link>
 
         <Link to="/capvert">
-            <div className='capvert'>
+            <div className='capvert imgs'>
                 <img src={logo2} alt="capvert" />
                 <p>Sabura life</p>
             </div>

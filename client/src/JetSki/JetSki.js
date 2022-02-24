aa1a9a265989a063bba5602881bd63121e194867
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-02-24 20:38:42 +0100

    jetskiwip

--- client/src/JetSki/JetSki.js
+++ client/src/JetSki/JetSki.js
@@ -1,31 +1,59 @@
 //real cmpnt
 import React from 'react';
 import "./JetSki.css";
-import logo from "./Kawasaki.jpeg";
-import logo1 from "./seddoo2.jpeg";
-import logo2 from "./yamaha.jpeg";
+import axios from "axios";
+import {CircularProgress} from "react-cssfx-loading";
 
 //component jetski
 class JetSki extends React.Component {
     state = {
         userId: "",
-        jetskiId: ""
+        jetskiId: "",
+        jetskiList:[]
     };
 
     componentDidMount = () => {
-        //axios
+        axios
+        .get("http://localhost:5005/api/jetski")
+        .then((response) =>{
+            this.setState({
+                    jetskiList: response.data.jetskiList,
+            });
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
         
     }
-    render() {
+    render = () => {
+        if (this.state.jetskiList.length <= 0){
+            return (
+               
+                <>
+                  <div className="loading-container" style={{display: "flex"}}>
+                      <CircularProgress style={{alignItems: "center", margin: "auto"}}/>
+                  </div>
+                </>
+              )
+
+        }
         return(
           <>
-                <h1>LOCATIONS</h1>
-                <div className="thejets">
-                    <img src={logo} alt="Kawasaki" />
-                        <p>Kawasaki</p>
-                        <p>auto gearbox 120 km/h</p>
+                 <h1>LOCATIONS</h1>
+               {
+                this.state.jetskiList.map((jetski) => {
+                return(
+                    <div key={jetski._id}>
+                    <img src={jetski.photo} alt={jetski.make} />
+                        <h4>{jetski.make}</h4>
+                        <p>{jetski.description}</p>
                         <button>Reservez</button>
-                </div>
+                    </div>
+                )
+            })
+               }
+               
+               
           </>
         );
     };

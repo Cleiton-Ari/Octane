885756a7d4812e46f2ee37b339f18a41b01715bd
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-02-24 18:48:57 +0100

    WIP

--- client/src/Jetski/Jetski.js
+++ client/src/Jetski/Jetski.js
@@ -6,6 +6,39 @@
             //                          {Image: database}
                     //                  {text (name, make, description): database}
                      //                 {button (ID jetski, ID user) => reservation }   
-
+                     import React from 'react';
+                     import "./JetSki.css";
+                     import logo from "./Kawasaki.jpeg";
+                     import logo1 from "./seddoo2.jpeg";
+                     import logo2 from "./yamaha.jpeg";
+                     
+                     //component jetski
+                     class JetSki extends React.Component {
+                         state = {
+                             userId: "",
+                             jetskiId: ""
+                         };
+                     
+                         componentDidMount = () => {
+                             //axios
+                             
+                         }
+                         render() {
+                             return(
+                               <>
+                                     <h1>LOCATIONS</h1>
+                                     <div className="thejets">
+                                         <img src={logo} alt="Kawasaki" />
+                                             <p>Kawasaki</p>
+                                             <p>auto gearbox 120 km/h</p>
+                                             <button>Reservez</button>
+                                     </div>
+                               </>
+                             );
+                         };
+                     }
+                     
+                     
+                     export default JetSki;
 
                      

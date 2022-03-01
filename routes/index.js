8bbc7c5f13686f9397b5aad75d2eaf3cecba53ce
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-03-01 21:21:59 +0100

    testing

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const authRoutes = require("./auth");
 
 // /* GET home page */
-// router.get("/", (req, res, next) => {
-//   res.json("All good in here");
-// });
-// router.use("/auth", authRoutes);
+router.get("/", (req, res, next) => {
+   res.json("All good in here");
+ });
+/ router.use("/auth", authRoutes);
 
 
 
